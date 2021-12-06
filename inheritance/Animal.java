7ea3909720f3d57507d987b2387c7b8cb5caf8eb
QA2021Oct <92875352+QA2021Oct@users.noreply.github.com>
2021-12-06 19:28:23 +0200

    Add files via upload

---
+++ inheritance/Animal.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Animal {
+    String name;
+    int age;
+    String color;
+
+    public Animal(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+}
---
+++ inheritance/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // inheritance:
+        Dog d = new Dog("dog", 3, "red");
+        System.out.println(d);
+    }
+}
